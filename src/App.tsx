875c302c6ee4e40822635e8386bc4f1b4a7aa694
isneru <diogoclanogueira@gmail.com>
2022-06-23 00:21:44 +0100

    Lesson 03

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,16 @@
-import { gql, useQuery } from "@apollo/client";
-
-import { Event } from "./pages/Event";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-      availableAt
-      lessonType
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-  availableAt: Date;
-  lessonType: string;
-}
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
+import { client } from "./lib/apollo";
 
 function App() {
-  return <Event />;
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
 export default App;
